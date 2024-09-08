338da0744d75b60962b5b4fc644cef6d2e8a31fd
v1c4r10us <v1c4r10us.d3v@gmail.com>
2024-09-08 03:25:22 +0000

    init-server modified

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM node:18-alpine
 
 WORKDIR /app
 
-COPY ./node_modules /app/node_modules 
 COPY package*.json /app/
 COPY ./public /app/public
 COPY ./src /app/src
--- init-server.sh
+++ init-server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker build -t server-label .
 docker run -it -d -p 3000:3000 --name reactlabel -v ./resources/files:/app/public/files server-label sh -c "npm start"
